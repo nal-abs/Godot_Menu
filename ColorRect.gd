95f5333a48393d2f7979816aff6307f5b1afb6fd
nal-abs <nal@abstractit.in>
2023-11-22 16:52:42 +0530

    Added Pause Screen

---
+++ ColorRect.gd
@@ -0,0 +1,9 @@
+extends ColorRect
+
+
+func _on_Back_pressed():
+	get_tree().change_scene_to(load("res://GameScreen.tscn"))
+
+	
+func _on_Home_pressed():
+		get_tree().change_scene_to(load("res://Main menu.tscn"))
--- GameScreen.gd
+++ GameScreen.gd
@@ -12,3 +12,9 @@ func _texture_react():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
+
+func _on_Pause_pressed():
+	get_tree().change_scene_to(load("res://PauseScreen.tscn"))
