258d640fbc4c91e5b03a171d92bb4ab20fea74d9
nal-abs <nal@abstractit.in>
2023-11-23 17:30:51 +0530

    Introduced different background position based on toggling.

--- ColorRect.gd
+++ ColorRect.gd
@@ -2,7 +2,7 @@ extends ColorRect
 
 
 func _on_Back_pressed():
-	get_tree().change_scene_to(load("res://GameScreen.tscn"))
+	get_tree().change_scene_to(load("res://spriteScene.tscn"))
 
 	
 func _on_Home_pressed():
